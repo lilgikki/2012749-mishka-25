f4a0110b6eed4d6b7339d161edf8b2bee0f59446
lilgikki <katy.kostyleva2010@gmail.com>
2022-05-06 17:15:22 +0300

    Decorative elements index

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--close')) {
+    navMain.classList.remove('main-nav--close');
+    navMain.classList.add('main-nav--open');
+  } else {
+    navMain.classList.add('main-nav--close');
+    navMain.classList.remove('main-nav--open');
+  }
+});
