17adf33ababee16688ab85506bb6056602cce3b9
lilgikki <katy.kostyleva2010@gmail.com>
2022-05-26 00:41:03 +0300

    tried

--- docs/js/modal.js
+++ docs/js/modal.js
@@ -1,17 +1,1 @@
-const buttonBuy = document.querySelector('.weekly-item__buy');
-const modal = document.querySelector('.modal');
-
-modal.classList.remove('modal--nojs');
-
-buttonBuy.addEventListener('click', function () {
-    modal.classList.remove('modal--noactive');
-    modal.classList.add('modal--active');
-});
-
-modal.addEventListener('click', e => {
-  const target = e.target;
-  if (!target.closest('.modal__container')) {
-    modal.classList.remove('modal--active');
-    modal.classList.add('modal--noactive');
-  }
-});
+const buttonBuy=document.querySelector(".weekly-item__buy"),modal=document.querySelector(".modal");modal.classList.remove("modal--nojs"),buttonBuy.addEventListener("click",(function(){modal.classList.remove("modal--noactive"),modal.classList.add("modal--active")})),modal.addEventListener("click",(e=>{e.target.closest(".modal__container")||(modal.classList.remove("modal--active"),modal.classList.add("modal--noactive"))}));
--- docs/js/nav.js
+++ docs/js/nav.js
@@ -1,14 +1,1 @@
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--close')) {
-    navMain.classList.remove('main-nav--close');
-    navMain.classList.add('main-nav--open');
-  } else {
-    navMain.classList.add('main-nav--close');
-    navMain.classList.remove('main-nav--open');
-  }
-});
+const navMain=document.querySelector(".main-nav"),navToggle=document.querySelector(".main-nav__toggle");navMain.classList.remove("main-nav--nojs"),navToggle.addEventListener("click",(function(){navMain.classList.contains("main-nav--close")?(navMain.classList.remove("main-nav--close"),navMain.classList.add("main-nav--open")):(navMain.classList.add("main-nav--close"),navMain.classList.remove("main-nav--open"))}));
